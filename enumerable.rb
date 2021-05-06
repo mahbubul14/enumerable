9f67193cd8f36168b1ebafa384c30e78ecd31d8d
mahbubul14 <mahbub21alam2017@gmailcom>
2021-05-07 02:43:53 +0600

    Fix some error

--- enumerable.rb
+++ enumerable.rb
@@ -1,9 +1,8 @@
 module Enumerable
-  def my_each
+  def my_each(&block)
     return to_enum unless block_given?
-    for list in self
-      yield list
-    end
+
+    each(&block)
   end
 
   def my_each_with_index
@@ -72,35 +71,30 @@ module Enumerable
   def my_map(proc = nil)
     new_arr = []
     if proc
-      my_each { |ele| new_arr << proc.call(ele) }
+      my_each { |list| new_arr << proc.call(list) }
     elsif block_given?
-      my_each { |ele| new_arr << yield(ele) }
+      my_each { |list| new_arr << yield(list) }
     else
       return to_enum unless block_given?
     end
     new_arr
   end
 
-  def my_inject(num = nil, sym = nil)
-    if sym.instance_of?(Symbol) || sym.instance_of?(String)
-      result = num
-      my_each do |item|
-        result = result.nil? ? item : result.send(sym, item)
-      end
-      result
-    elsif num.instance_of?(Symbol) || num.instance_of?(String)
-      result = nil
-      my_each do |item|
-        result = result.nil? ? item : result.send(num, item)
-      end
-      result
+  def my_inject(para1 = nil, para2 = nil)
+    array = to_a
+    acc = para1 || array[0]
+    i = para1 ? 0 : 1
+    if block_given?
+      (i...array.length).each { |idx| acc = yield(acc, array[idx]) }
+    elsif para1 && para2
+      (i...array.length).each { |idx| acc = acc.send(para2, array[idx]) }
+    elsif para1
+      acc = array[0]
+      (1...array.length).each { |idx| acc = acc.send(para1, array[idx]) }
     else
-      result = num
-      my_each do |item|
-        result = result.nil? ? item : yield(result, item)
-      end
+      raise LocalJumpError
     end
-    result
+    acc
   end
 end
 
