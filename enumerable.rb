6e36cd7f29af91ad668711887b3377438d34f3c3
mahbubul14 <mahbub21alam2017@gmailcom>
2021-05-07 02:37:16 +0600

    Fix the changes

--- enumerable.rb
+++ enumerable.rb
@@ -1,12 +1,13 @@
 module Enumerable
-  def my_each(&block)
-    return unless block_given?
-
-    each(&block)
+  def my_each
+    return to_enum unless block_given?
+    for list in self
+      yield list
+    end
   end
 
   def my_each_with_index
-    return unless block_given?
+    return to_enum unless block_given?
 
     index = 0
     each do |i|
@@ -23,55 +24,86 @@ module Enumerable
     new_arr = []
   end
 
-  def my_all?(&block)
-    return to_enum unless block_given?
-
-    result = my_select(&block)
-    length == result.length
-  end
-
-  def my_any?(&block)
-    return to_enum unless block_given?
-
-    result = my_select(&block)
-    result.length.positive?
-  end
-
-  def my_none?
-    return to_enum unless block_given?
-
-    !my_any?
-  end
-
-  def my_count(&block)
-    return count if count
-    return length unless block_given?
-
-    my_select(&block).length
-  end
-
-  def my_map(my_proc = nil)
-    array = []
-    my_each { |x| array << my_proc.call(x) } if my_proc
-    my_each { |x| array << yield(x) } if block_given?
-    array
-  end
-
-  def my_inject(*args)
-    case args.length
-    when 1 then args.first.is_a?(Symbol) ? sym = args.first : result = args.first
-    when 2 then result = args.first
-                sym = args.last
+  def my_all?(para = nil)
+    if block_given?
+      my_each { |list| return false unless yield list }
+    elsif para
+      my_each { |list| return false unless match?(list, para) }
+    else
+      my_each { |list| return false unless list }
     end
+    true
+  end
 
-    result ||= 0
+  def my_any?(para = nil)
+    if block_given?
+      my_each { |ele| return true if yield ele }
+    elsif para
+      my_each { |ele| return true if match?(ele, para) }
+    else
+      my_each { |ele| return true if ele }
+    end
+    false
+  end
 
-    my_each { |x| result = block_given? ? yield(result, x) : result.send(sym, x) }
+  def my_none?(para = nil)
+    if block_given?
+      my_each { |ele| return false if yield ele }
+    elsif para
+      my_each { |ele| return false if match?(ele, para) }
+    else
+      my_each { |ele| return false if ele }
+    end
+    true
+  end
 
+  def my_count(para = nil)
+    count = 0
+    if block_given?
+      my_each { |ele| count += 1 if yield ele }
+    elsif para
+      my_each { |ele| count += 1 if match?(ele, para) }
+    else
+      return length
+    end
+    count
+  end
+
+  def my_map(proc = nil)
+    new_arr = []
+    if proc
+      my_each { |ele| new_arr << proc.call(ele) }
+    elsif block_given?
+      my_each { |ele| new_arr << yield(ele) }
+    else
+      return to_enum unless block_given?
+    end
+    new_arr
+  end
+
+  def my_inject(num = nil, sym = nil)
+    if sym.instance_of?(Symbol) || sym.instance_of?(String)
+      result = num
+      my_each do |item|
+        result = result.nil? ? item : result.send(sym, item)
+      end
+      result
+    elsif num.instance_of?(Symbol) || num.instance_of?(String)
+      result = nil
+      my_each do |item|
+        result = result.nil? ? item : result.send(num, item)
+      end
+      result
+    else
+      result = num
+      my_each do |item|
+        result = result.nil? ? item : yield(result, item)
+      end
+    end
     result
   end
+end
 
-  def multiply_els
-    my_inject(1, :*)
-  end
+def multiply_els(array)
+  array.my_inject(1) { |index, result| result * index }
 end
